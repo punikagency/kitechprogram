03c213dd5d9fa8dab838059c7c433f6ed2d32805
mrpaulishaili <paulishaili@gmail.com>
2023-07-02 08:14:40 +0100

    ... first push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
+});
